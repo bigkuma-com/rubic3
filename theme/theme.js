4fd1248d967d3de9a8be4fc8088c70036ff899a4
arridhow <arridhow@gmail.com>
2022-11-07 13:04:23 +0700

    :rocket: initial commit

---
+++ theme/theme.js
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Domine', sans-serif`,
+    body: `'Questrial', sans-serif`,
+  },
+});
+
+export default theme;
