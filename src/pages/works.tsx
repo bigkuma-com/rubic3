f96c69c497e2bbb4728af96151f1971101607883
arridhow <arridhow@gmail.com>
2022-11-21 14:59:11 +0700

    :construction: [WIP]

--- src/pages/works.tsx
+++ src/pages/works.tsx
@@ -1,5 +1,44 @@
 import { Box } from "@chakra-ui/react";
+import { useState } from "react";
+import BoxMotion from "../components/BoxMotion";
 
 export default function Works() {
-  return <Box h="100vh" w="full" bg="pink.700"></Box>;
+  const [mousePosition, setMousePosition] = useState({
+    left: 0,
+    top: 0,
+  });
+
+  function handleMouseMove(ev: { pageX: any; pageY: any }) {
+    setMousePosition({ left: ev.pageX, top: ev.pageY });
+  }
+
+  console.log(mousePosition);
+
+  function onPan(event: any, info: { point: { x: any; y: any } }) {
+    console.log(info.point.x, info.point.y);
+    console.log("ha", event.pageX, event.pageY);
+  }
+
+  return (
+    <Box h="100vh" w="100vw">
+      <BoxMotion
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        h="150vh"
+        w="150vw"
+        bg="pink.700"
+        animate={{
+          y: -200,
+        }}
+        onPan={onPan}
+        // onMouseMove={(ev) => {
+        //   // console.log(ev.currentTarget.hei)
+        //   handleMouseMove(ev);
+        // }}
+      >
+        <Box h="100vh" w="100vw" bg="yellow.500"></Box>
+      </BoxMotion>
+    </Box>
+  );
 }
