c6d1fbfec9f9147a56926118a97e6fb285633c72
arridhow <arridhow@gmail.com>
2022-12-13 12:55:48 +0700

    :pencil: other works

--- src/pages/works/[slug].tsx
+++ src/pages/works/[slug].tsx
@@ -171,14 +171,7 @@ export default function Work({ work }: any) {
                 >
                   <Text mb={[2, 3, 4, 6]}>Other Works</Text>
                   <Box display="flex" flexDirection="column" gap={7}>
-                    {other_works.map((other: any, i: any) => {
-                      console.log(
-                        getImage({
-                          collectionName: other.collectionName,
-                          recordId: other.id,
-                          filename: other.showcase,
-                        })
-                      );
+                    {other_works?.map((other: any, i: any) => {
                       return (
                         <Box
                           key={i}
@@ -215,39 +208,44 @@ export default function Work({ work }: any) {
                       );
                     })}
                   </Box>
-                  <Box
-                    position="absolute"
-                    top="50%"
-                    right={marginX}
-                    transform="translate(-50%, -50%)"
-                    zIndex={50}
-                    color="light"
-                  >
+
+                  {next_work && (
                     <Box
-                      display="flex"
-                      flexDirection="column"
-                      alignItems="center"
-                      gap={2}
-                      cursor="pointer"
-                      color="inherit"
-                      onClick={() => push(`/works/${next_work.slug}`)}
+                      position="absolute"
+                      top="50%"
+                      right={marginX}
+                      transform="translate(-50%, -50%)"
+                      zIndex={50}
+                      color="light"
                     >
-                      <Text
+                      <Box
+                        display="flex"
+                        flexDirection="column"
+                        alignItems="center"
+                        gap={2}
+                        cursor="pointer"
                         color="inherit"
-                        as="span"
-                        style={{
-                          writingMode: "vertical-lr",
-                          transform: "rotate(0deg)",
+                        onClick={() => {
+                          push(`/works/${next_work.slug}`);
                         }}
-                        fontSize="small"
-                        letterSpacing="wider"
-                        fontWeight={400}
                       >
-                        Next Works
-                      </Text>
-                      <ArrowRightSm />
+                        <Text
+                          color="inherit"
+                          as="span"
+                          style={{
+                            writingMode: "vertical-lr",
+                            transform: "rotate(0deg)",
+                          }}
+                          fontSize="small"
+                          letterSpacing="wider"
+                          fontWeight={400}
+                        >
+                          Next Works
+                        </Text>
+                        <ArrowRightSm />
+                      </Box>
                     </Box>
-                  </Box>
+                  )}
                 </Box>
               </SwiperSlide>
             </Swiper>
@@ -286,101 +284,70 @@ export default function Work({ work }: any) {
               </Box>
             );
           })}
-          <Box
-            h="full"
-            w="full"
-            position="relative"
-            display="flex"
-            flexDirection="column"
-            justifyContent="center"
-            pl={[5, 6, 10, 16]}
-            pr={[5, 6, 10, 0]}
-            mt={[6, 6, 6, 0]}
-            mb={[14, 14, 14, 0]}
-          >
-            <Text mb={[2, 3, 4, 6]}>Other Works</Text>
+          {other_works?.length > 0 && (
             <Box
+              h="full"
+              w="full"
+              position="relative"
               display="flex"
-              flexDirection={{ base: "row", lg: "column" }}
-              gap={7}
+              flexDirection="column"
+              justifyContent="center"
+              pl={[5, 6, 10, 16]}
+              pr={[5, 6, 10, 0]}
+              mt={[6, 6, 6, 0]}
+              mb={[14, 14, 14, 0]}
             >
-              {other_works.map((other: any, i: any) => {
-                return (
-                  <Box
-                    w={{ base: "full", lg: "unset" }}
-                    key={i}
-                    cursor="pointer"
-                    onClick={() => {
-                      push(`/works/${other.slug}`);
-                    }}
-                  >
-                    <Box
-                      position="relative"
-                      w={{ base: "full", lg: "60%" }}
-                      h={{ base: "20vh", lg: "25vmin" }}
-                      mb={2}
-                    >
-                      <Image
-                        src={getImage({
-                          collectionName: other.collectionName,
-                          recordId: other.id,
-                          filename: other.thumbnail,
-                        })}
-                        alt={other.name}
-                        fill
-                        placeholder="blur"
-                        blurDataURL={`/_next/image?url=${getImage({
-                          collectionName: other.collectionName,
-                          recordId: other.id,
-                          filename: other.thumbnail,
-                        })}&w=16&q=1`}
-                        style={{
-                          objectFit: "cover",
-                          objectPosition: "center center",
-                        }}
-                      />
-                    </Box>
-                    <Text opacity={0.7} fontSize="sm">
-                      {other.name}
-                    </Text>
-                  </Box>
-                );
-              })}
-            </Box>
-            <Box
-              position="absolute"
-              top="50%"
-              right={marginX}
-              transform="translate(-50%, -50%)"
-              zIndex={50}
-              color="light"
-            >
+              <Text mb={[2, 3, 4, 6]}>Other Works</Text>
               <Box
-                display={{ base: "none", lg: "flex" }}
-                flexDirection="column"
-                alignItems="center"
-                gap={2}
-                cursor="pointer"
-                color="inherit"
-                onClick={() => push(`/works/${next_work.slug}`)}
+                display="flex"
+                flexDirection={{ base: "row", lg: "column" }}
+                gap={7}
               >
-                <Text
-                  color="inherit"
-                  as="span"
-                  style={{
-                    writingMode: "vertical-lr",
-                    transform: "rotate(0deg)",
-                  }}
-                  fontSize="small"
-                  letterSpacing="wider"
-                  fontWeight={400}
-                >
-                  Next Works
-                </Text>
-                <ArrowRightSm />
+                {other_works.map((other: any, i: any) => {
+                  return (
+                    <Box
+                      w={{ base: "full", lg: "unset" }}
+                      key={i}
+                      cursor="pointer"
+                      onClick={() => {
+                        push(`/works/${other.slug}`);
+                      }}
+                    >
+                      <Box
+                        position="relative"
+                        w={{ base: "full", lg: "60%" }}
+                        h={{ base: "20vh", lg: "25vmin" }}
+                        mb={2}
+                      >
+                        <Image
+                          src={getImage({
+                            collectionName: other.collectionName,
+                            recordId: other.id,
+                            filename: other.thumbnail,
+                          })}
+                          alt={other.name}
+                          fill
+                          placeholder="blur"
+                          blurDataURL={`/_next/image?url=${getImage({
+                            collectionName: other.collectionName,
+                            recordId: other.id,
+                            filename: other.thumbnail,
+                          })}&w=16&q=1`}
+                          style={{
+                            objectFit: "cover",
+                            objectPosition: "center center",
+                          }}
+                        />
+                      </Box>
+                      <Text opacity={0.7} fontSize="sm">
+                        {other.name}
+                      </Text>
+                    </Box>
+                  );
+                })}
               </Box>
             </Box>
-          </Box>
+          )}
         </Box>
       )}
       <Footer />
