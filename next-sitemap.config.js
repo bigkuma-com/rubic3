abfac126788696792597b09065b91c7e798bee10
arridhow <arridhow@gmail.com>
2023-02-03 13:15:55 +0700

    :pencil: SEO 7 analytics

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://rubic3.com",
+  generateRobotsTxt: true,
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,7 @@ import "@fontsource/roboto/700.css";
 import "@fontsource/roboto/900.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Script from "next/script";
 import NextNProgress from "nextjs-progressbar";
 import "../assets/css/main.scss";
 import theme from "../theme/theme";
@@ -20,7 +21,25 @@ function MyApp({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
+        <meta
+          name="google-site-verification"
+          content="YIX-d0c9CMXch52a2X_wFMYqjpxyKgUV0vnsBabfqWE"
+        />
       </Head>
+      <Script
+        async
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+      </Script>
       <ChakraProvider theme={theme}>
         <NextNProgress
           color="#ffffff"
