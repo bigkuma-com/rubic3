ba9b2110ce4acaf8bb09d0fec8241a43a528329e
arridhow <arridhow@gmail.com>
2023-05-26 16:27:16 +0700

    add analytics

---
+++ src/container/Analytics.tsx
@@ -0,0 +1,47 @@
+import Head from "next/head";
+import Script from "next/script";
+
+export default function Analytics() {
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+        <meta
+          name="google-site-verification"
+          content="YIX-d0c9CMXch52a2X_wFMYqjpxyKgUV0vnsBabfqWE"
+        />
+      </Head>
+      <Script
+        async
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+      </Script>
+      <Script
+        async
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_PROD}`}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_PROD}');
+        `}
+      </Script>
+    </>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,39 +7,15 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "@fontsource/roboto/900.css";
 import type { AppProps } from "next/app";
-import Head from "next/head";
-import Script from "next/script";
 import NextNProgress from "nextjs-progressbar";
 import "../assets/css/main.scss";
+import Analytics from "../container/Analytics";
 import theme from "../theme/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-        />
-        <meta
-          name="google-site-verification"
-          content="YIX-d0c9CMXch52a2X_wFMYqjpxyKgUV0vnsBabfqWE"
-        />
-      </Head>
-      <Script
-        async
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-        `}
-      </Script>
+      <Analytics />
       <ChakraProvider theme={theme}>
         <NextNProgress
           color="#ffffff"
