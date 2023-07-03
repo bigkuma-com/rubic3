33c18ab6027043caa972ea64c4e26111bb7fa5d4
arridhow <arridhow@gmail.com>
2023-07-03 07:12:24 +0700

    :pencil: position & image

--- src/components/about/Section2.tsx
+++ src/components/about/Section2.tsx
@@ -59,12 +59,12 @@ export default function Section2({
             "Property & Hospitality",
             "Retail. Lifestyle & Place Making",
             "Products & Services",
-            "Associations",
             "Industrial & Distribution",
             "Creative & Entertainment",
             "Property & Placemaking",
             "Food & Beverages",
             "Retail & E-Commerce",
+            "Associations",
           ].map((title, i) => {
             if (!partnersAssociations[title]) return null;
             return (
@@ -74,8 +74,8 @@ export default function Section2({
                   color="dark"
                   mb={6}
                 />
-                {title == "Products & Services" ||
-                title == "Industrial & Distribution" ? (
+                {((title == "Products & Services" ||
+                  title == "Industrial & Distribution") && (partnersAssociations[title][0].is_text_only)) ? (
                   <SimpleGrid columns={[2]} mt={2} mb={8} columnGap={4}>
                     {partnersAssociations[title].map(
                       ({
